157e5e9b6431266eec98a5d67857b065a5db0fa1
zhu2688 <zhu2688@gmail.com>
2020-03-14 14:14:51 +0800

    vm_check.sh

--- vm_check.sh
+++ vm_check.sh
@@ -1,7 +1,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 export PATH
 yum install -y gcc gcc-c++ gdb
-wget https://github.com/zhu2688/code/releases/download/0.0.1/virt-what-1.12.tar.gz
+wget https://github.com/zhu2688/code/releases/download/0.0.1/virt-what-1.20.tar.gz
 tar zxvf virt-what-1.12.tar.gz
 cd virt-what-1.12/
 ./configure
